c10a4d126a75654f4a9cba682e6e2c59d7377482
Felipe Dalperio <45234913+felipedalperio@users.noreply.github.com>
2023-01-17 19:32:10 -0300

    footer and tech

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -8,17 +8,17 @@ export default function Tech() {
         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod illo iusto eveniet rem voluptatem maiores ratione dignissimos obcaecati alias quis molestiae saepe quasi, aperiam necessitatibus, accusamus laudantium minus laboriosam magni?</span>
         <div className="wrapperTech">
             <div className="iconTech">
-                <FaReact size={60} style={{color:'#89D790'}}/>
+                <FaReact size={40} style={{color:'#89D790'}}/>
                 <span>Front-End</span>
                 <span>React.js e Next.js</span>
             </div>
             <div className="iconTech">
-                <FaCode size={60} style={{color:'#89D790'}}/>
+                <FaCode size={40} style={{color:'#89D790'}}/>
                 <span>Back-end</span>
                 <span>Java, Javascript e Node</span>
             </div>
             <div className="iconTech">
-                <FaDatabase size={60} style={{color:'#89D790'}}/>
+                <FaDatabase size={40} style={{color:'#89D790'}}/>
                 <span>Database</span>
                 <span>Firebase, MongoDB, MySql</span>
             </div>
