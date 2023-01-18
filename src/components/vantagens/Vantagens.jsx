650a8bce1457eb1818c47a9c4ba7cd5abb199c8d
Felipe Dalperio <45234913+felipedalperio@users.noreply.github.com>
2023-01-18 14:40:25 -0300

    animation

--- src/components/vantagens/Vantagens.jsx
+++ src/components/vantagens/Vantagens.jsx
@@ -1,9 +1,18 @@
-import './vantagens.css'
+import './vantagens.css';
+import { useContext, useState } from 'react';
+import { ScrollContext } from '../../context/ScrollContext';
 
 export default function Vantagens() {
+  const {scrollContext} = useContext(ScrollContext)
+  const[pass,setPass] = useState(false);
+  if(scrollContext > 1500){
+      if(pass == false){
+          setPass(true)
+      }
+  }  
   return (
     <div className='vantagens'>
-        <div className="boxvantagem">
+        <div className="boxvantagem" style={{animationName: pass && 'showCard'}}>
             <div className="imageVantagem">
                 <div className="circle">
                     <img src="./images/planet.webp  " alt="" width={220} height={200} />
